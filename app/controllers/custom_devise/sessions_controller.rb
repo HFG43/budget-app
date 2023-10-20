9cda8f3fd2fdc5e0a3347e1842e77372e69f599f
HFG43 <hernanguemes@hotmail.com>
2023-10-19 23:08:26 -0300

    Feat: sign in view style

--- app/controllers/custom_devise/sessions_controller.rb
+++ app/controllers/custom_devise/sessions_controller.rb
@@ -2,4 +2,9 @@ class CustomDevise::SessionsController < Devise::SessionsController
   def after_sign_in_path_for(_resource)
     groups_path
   end
+
+  def new
+    @path_description = 'REGISTER'
+    super
+  end
 end
